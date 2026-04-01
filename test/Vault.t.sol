0a6f87d3533dad3d048012424ef775df1c19e525
yusuforcun <yusuf.orcun.ozd@gmail.com>
2026-04-01 11:54:14 +0300

    added funcs

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -85,6 +85,91 @@ contract VaultTimeTest is Test {
 
     function test_CannotWithdrawBeforeLockPeriod() public {
         vm.prank(user);
-        vault.deposit{value:1ether}();
+        vault.deposit{value:1 ether}();
+        wm.warp(1000+6 days);
+        wm.prank(user);
+        wm.expectRevert("Locked");
+        vault.withdraw(1 ether);
     }
+
+    function test_WithdrawAtDifferentBlocks() public {
+
+        vm.prank(user);
+        vault.deposit{value : 1 ether};
+
+        vm.roll(100);
+        vm.prank(user);
+        vm.expectRevert("Locked");
+        vault.withdraw(1 ether);
+
+        vm.warp(1000+8days);
+        vm.roll(1000);
+
+        vm.prank(user);
+        vault.withdraw(1 ether);
+
+        assertEq(vault.balances(user),0);
+    }
+}
+
+contract VaultEventTest is Test{
+    Vault public vault ;
+    address user = makeAddr("user");
+
+    function setUp() public {
+        vault = new Vault();
+        vm.deal(user,10 ether);
+        vm.warp(1000);
+    }
+
+    function test_DepositEmitsEvent() public {
+        vm.prank(user);
+
+        vm.expectEmit(true,false,false,true);
+        emit Deposited(user:1 ether);
+
+        vault.deposit{value: 1 ether}();
+    }
+
+    function test_WithdrawEmitsEvent() public {
+        vm.prank(user);
+        vault.deposit{value:1 ether}();
+
+        vm.warp(1000+8 days);
+
+        vm.prank(user);
+
+        vm.expectEmit(true , false , false , true);
+        emit Withdrawn(user,0.5 ether);
+
+        vault.withdraw(0.5 ether);
+    }
+}
+
+contract VaultSnapshotTest is Test {
+    Vault public vault ;
+    address user = makeAddr("user");
+
+    function setUp() public {
+        vault = new Vault();
+        vm.deal(user,10 ether);
+        vm.warp(1000);
+    }
+
+    function test_SnapshotAndRevert() public {
+        assertEq(address(vault).balance,0);
+
+        uint256 snapshotId = vm.snapshot();
+
+        vm.prank(user);
+        vault.deposit{value : 1 ether}();
+        assertEq(address(vault).balance, 1 ether);
+
+        vm.revertTo(snapshotId);
+
+        assertEq(address(vault).balance,0);
+        assertEq(vault.balances(user),0);
+    }
+
+    function test_MultipleScenarios()public{}
 }
